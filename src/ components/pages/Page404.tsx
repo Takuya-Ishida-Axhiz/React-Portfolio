e45f4ddf03228c271ca84d1fe2d5ad044875255d
Takuya-Ishida-Axhiz <axhiz.ozi@gmail.com>
2022-03-07 09:24:31 +0900

    自動デプロイプッシュ

--- src/ components/pages/Page404.tsx
+++ src/ components/pages/Page404.tsx
@@ -1,6 +1,11 @@
-import { memo } from "react";
-import { VFC } from "react";
+import { Center } from '@chakra-ui/react'
+import { memo } from 'react'
+import { VFC } from 'react'
 
-export const Page404 :VFC= memo(()=>{
-    return <p>404ページです</p>
+export const Page404: VFC = memo(() => {
+  return (
+    <Center>
+      <p>404ページです</p>
+    </Center>
+  )
 })
